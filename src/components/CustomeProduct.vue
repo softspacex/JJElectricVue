96f4e76ce3ecccbf0790ba3625c17573b3116d99
softspacex <150283411+softspacex@users.noreply.github.com>
2024-06-07 00:01:56 -0400

    Terminando producto page

---
+++ src/components/CustomeProduct.vue
@@ -0,0 +1,78 @@
+<template>
+    <div class="product ">
+      <img :src="url" alt="" />
+      <div class="product-info">
+        <h4 class="product-title text-uppercase">{{ title }}</h4>
+        <a :href="pdfUrl" class="btn text-uppercase" target="_blank">Ver catálogo</a>
+      </div>
+    </div>
+  </template>
+  
+  <script setup>
+  import { defineProps } from "vue";
+  
+  const props = defineProps({
+    title: String,
+    url: String,
+    pdfUrl: String
+  });
+  </script>
+  
+  <style scoped>
+  .product {
+    overflow: hidden;
+    background: #ffffff;
+    color: #21201e;
+    text-align: center;
+    padding: 2rem;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    border-radius: 1.2rem;
+    transition: transform 0.3s;
+  }
+  
+  .product:hover {
+    box-shadow: 5px 15px 25px #eeeeee;
+    transform: scale(1.05);
+  }
+  
+  .product img {
+    height: 200px;
+    margin: 1rem;
+    transition: all 0.3s;
+  }
+  
+  h4 {
+    font-size: 15px;
+    color: var(--middle-grey);
+  }
+  .btn {
+  margin-top: 1rem;
+  display: inline-block;
+  font-size: 1.7rem;
+  color: #fff;
+  background: var(--dark-blue);
+  border-radius: 0.5rem;
+  cursor: pointer;
+  padding: 0.8rem 3rem;
+}
+
+.btn:hover {
+  background: var(--light-yellow);
+  letter-spacing: 0.1rem;
+}
+@media (max-width: 768px) {
+  .product {
+    width: 100%; /* Elimina restricciones de ancho para ocupar todo el ancho disponible */
+    padding: 1rem; /* Ajusta padding si es necesario */
+  }
+
+  .product img {
+    height: auto; /* Ajusta la altura para mantener la proporción */
+    max-width: 100%; /* Asegura que la imagen no exceda el contenedor */
+  }
+}
+  </style>
+  
--- src/views/Productos.vue
+++ src/views/Productos.vue
@@ -1,6 +1,21 @@
 <template>
   <default-layout></default-layout>
 
+  <article class="text-prod text-center">
+    <h1 class="text-uppercase text-principal">Nuestros Productos</h1>
+    <h3 class="text-uppercase text-secundary">Todo lo que necesitas Para tu Proyecto</h3>
+  </article>
+
+  <article class="product-list">
+    <custome-product 
+      v-for="(product, index) in products" 
+      :key="index"
+      :title="product.title"
+      :url="product.url"
+      :pdfUrl="product.pdfUrl"
+    ></custome-product>
+  </article>
+
   <article>
     <the-footer></the-footer>
   </article>
@@ -9,4 +24,44 @@
 <script setup>
 import DefaultLayout from "../layouts/DefaultLayout.vue";
 import TheFooter from "../components/TheFooter.vue";
+import CustomeProduct from "../components/CustomeProduct.vue";
+
+const products = [
+  { title: "Iluminación", url: "/Product-1.jpg", pdfUrl: "https://drive.google.com/file/d/18hLrwT2O2a990Ic-fuyEh4r1k2hM6qfA/view?usp=sharing" },
+  { title: "Media y Alta Tensión", url: "/Product-2.jpg", pdfUrl: "https://drive.google.com/file/d/1OQH_OWwaWGsepAZhZP9o5PgTBKt5MDIG/view?usp=sharing" },
+  { title: "Cables y Accesorios", url: "/Product-3.jpg", pdfUrl: "https://drive.google.com/file/d/13gRnjOfjyoxHScH4VzHVLHTfcdhh4689/view?usp=sharing" },
+  { title: "Protección", url: "/Product-4.jpg", pdfUrl: "https://drive.google.com/file/d/1aYBIVYNtEIoDQ5i_N6AA6P26mJVaq1F7/view?usp=sharing" },
+  { title: "Enchufes y Tomacorrientes", url: "/Product-5.jpg", pdfUrl: "https://drive.google.com/file/d/1NU3FGrUzAT3FPA4vczB0SnYX8qcz4cJ4/view?usp=sharing" },
+  { title: "Fitting", url: "/Product-6.jpg", pdfUrl: "https://drive.google.com/file/d/1m3lodcr4SSsIqgNXzp8BShU1lF9qp0z-/view?usp=sharing" },
+  { title: "Tornillería", url: "/Product-7.jpg", pdfUrl: "https://drive.google.com/file/d/11R9C_GlGP9xLTT7U3GwjSKHcJr1yvCBH/view?usp=sharing" },
+  { title: "Herramientas y Accesorios", url: "/Product-8.jpg", pdfUrl: "https://drive.google.com/file/d/1Qb00LxZZYOrcRGYLeUCNyjlZadenRgIN/view?usp=sharing" },
+  { title: "Controles", url: "/Product-9.jpg", pdfUrl: "https://drive.google.com/file/d/1zbGemBokq-EiIvZEe2qMY_kCa7KRnc91/view?usp=sharing" },
+  { title: "Accesorios PVC", url: "/Product-10.jpg", pdfUrl: "https://drive.google.com/file/d/13v6VDm3QIm4zbLA5BvJD7yxa9lLOfYbE/view?usp=sharing" },
+];
 </script>
+
+<style scoped>
+.text-prod {
+  margin-top: 20rem;
+}
+.text-principal {
+  font-size: 6rem;
+}
+.text-secundary {
+  font-size: 2.5rem;
+  color: var(--middle-grey);
+}
+
+.product-list {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 2rem; /* Espacio entre productos */
+  padding: 2rem; /* Opcional: para espacio alrededor de la lista */
+}
+@media (max-width: 768px) {
+  .product-list {
+    grid-template-columns: 1fr; /* Una columna para dispositivos pequeños */
+    padding: 1rem; /* Ajusta el espacio de padding si es necesario */
+  }
+}
+</style>
